7fca6d47d93521e21abe4ac88d84999b712a6134
0000xFFFF <coldmailgg@gmail.com>
2025-03-10 21:34:41 +0100

    sfr

--- setup_configs.sh
+++ setup_configs.sh
@@ -3,5 +3,5 @@ mkdir -p ~/.config/i3
 cp configs/i3config ~/.config/i3/config
 mkdir -p ~/.config/alacritty
 cp configs/alacritty.yml ~/.config/alacritty/alacritty.yml
-ln -sf static/fonts/Gohu ~/.local/share/fonts/Gohu
+ln -sfr static/fonts/Gohu ~/.local/share/fonts/Gohu
 fc-cache -r
