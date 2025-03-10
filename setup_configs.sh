c876e522f32ec4b3e61d49a697071f308873941a
0000xFFFF <coldmailgg@gmail.com>
2025-03-10 22:10:02 +0100

    toml

--- setup_configs.sh
+++ setup_configs.sh
@@ -3,6 +3,7 @@ mkdir -p ~/.config/i3
 cp configs/i3config ~/.config/i3/config
 mkdir -p ~/.config/alacritty
 cp configs/alacritty.yml ~/.config/alacritty/alacritty.yml
+cp configs/alacritty.toml ~/.config/alacritty/alacritty.toml
 mkdir -p ~/.local/share/fonts
 ln -sfr static/fonts/Gohu ~/.local/share/fonts/Gohu
 fc-cache -r
