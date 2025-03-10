7ccc2275e2df7e8af867f8fa9230dbdb6aaade2e
0000xFFFF <coldmailgg@gmail.com>
2025-03-10 22:03:15 +0100

    local share fonts

--- setup_configs.sh
+++ setup_configs.sh
@@ -3,5 +3,6 @@ mkdir -p ~/.config/i3
 cp configs/i3config ~/.config/i3/config
 mkdir -p ~/.config/alacritty
 cp configs/alacritty.yml ~/.config/alacritty/alacritty.yml
+mkdir -p ~/.local/share/fonts
 ln -sfr static/fonts/Gohu ~/.local/share/fonts/Gohu
 fc-cache -r
