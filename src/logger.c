25f19ad15c4447f558bc5518b38762164300cf65
0000xFFFF <coldmailgg@gmail.com>
2025-09-17 11:45:23 +0200

    reformat logger.c clangd

--- src/logger.c
+++ src/logger.c
@@ -5,18 +5,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define LOG_CHANGES "changes.log"
+#define LOG_CHANGES  "changes.log"
 #define LOG_REQUESTS "requests.log"
-#define LOG_ERRORS "errors.log"
+#define LOG_ERRORS   "errors.log"
 
 // General logging function
-void logger_write(const char* filename, const char* fmt, va_list args) {
+void logger_write(const char* filename, const char* fmt, va_list args)
+{
     if (fmt == NULL || filename == NULL) { return; }
 
     FILE* f = fopen(filename, "a");
-    if (f == NULL) { 
+    if (f == NULL) {
         perror("Failed to open log file");
-        return; 
+        return;
     }
 
     char* t = get_current_time();
@@ -27,21 +28,24 @@ void logger_write(const char* filename, const char* fmt, va_list args) {
     fclose(f);
 }
 
-void logger_errors_write(const char* fmt, ...) {
+void logger_errors_write(const char* fmt, ...)
+{
     va_list args;
     va_start(args, fmt);
     logger_write(LOG_ERRORS, fmt, args);
     va_end(args);
 }
 
-void logger_requests_write(const char* fmt, ...) {
+void logger_requests_write(const char* fmt, ...)
+{
     va_list args;
     va_start(args, fmt);
     logger_write(LOG_REQUESTS, fmt, args);
     va_end(args);
 }
 
-void logger_changes_write(const char* fmt, ...) {
+void logger_changes_write(const char* fmt, ...)
+{
     va_list args;
     va_start(args, fmt);
     logger_write(LOG_CHANGES, fmt, args);
