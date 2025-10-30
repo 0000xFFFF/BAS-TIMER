6503a9d8969fdfe584cfdbdb748cce71b2deaa2b
0000xFFFF <coldmailgg@gmail.com>
2025-10-30 01:00:02 +0100

    fix .h file _H

--- src/serve_websocket.h
+++ src/serve_websocket.h
@@ -1,5 +1,5 @@
-#ifndef SERVE_WEBSOCKET
-#define SERVE_WEBSOCKET
+#ifndef SERVE_WEBSOCKET_H
+#define SERVE_WEBSOCKET_H
 
 #include <stdatomic.h>
 #include "mongoose.h"
@@ -8,4 +8,4 @@ extern atomic_int g_ws_conn_count;
 extern void websocket_emit(const char* data, int len);
 extern void serve_websocket(struct mg_connection* c, int ev, void* ev_data);
 
-#endif // SERVE_WEBSOCKET
+#endif // SERVE_WEBSOCKET_H
