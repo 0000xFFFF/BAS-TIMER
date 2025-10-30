29f15959591a1b9566a337fa425734689ea1a0a2
0000xFFFF <coldmailgg@gmail.com>
2025-10-30 15:43:08 +0100

    s_ are static

--- src/logger.c
+++ src/logger.c
@@ -5,9 +5,9 @@
 #include <stdio.h>
 #include <string.h>
 
-static pthread_mutex_t g_mutex_file_errors = PTHREAD_MUTEX_INITIALIZER;
-static pthread_mutex_t g_mutex_file_requests = PTHREAD_MUTEX_INITIALIZER;
-static pthread_mutex_t g_mutex_file_changes = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t s_mutex_file_errors = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t s_mutex_file_requests = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t s_mutex_file_changes = PTHREAD_MUTEX_INITIALIZER;
 
 void logger_write(const char* filename, const char* fmt, va_list args)
 {
@@ -30,41 +30,41 @@ void logger_write(const char* filename, const char* fmt, va_list args)
 void logger_errors_write(const char* fmt, ...)
 {
 
-    pthread_mutex_lock(&g_mutex_file_errors);
+    pthread_mutex_lock(&s_mutex_file_errors);
     va_list args;
     va_start(args, fmt);
     logger_write(STATE_DIR_FILE_ERRORS_LOG, fmt, args);
     va_end(args);
-    pthread_mutex_unlock(&g_mutex_file_errors);
+    pthread_mutex_unlock(&s_mutex_file_errors);
 }
 
 void logger_requests_write(const char* fmt, ...)
 {
-    pthread_mutex_lock(&g_mutex_file_requests);
+    pthread_mutex_lock(&s_mutex_file_requests);
     va_list args;
     va_start(args, fmt);
     logger_write(STATE_DIR_FILE_REQUESTS_LOG, fmt, args);
     va_end(args);
-    pthread_mutex_unlock(&g_mutex_file_requests);
+    pthread_mutex_unlock(&s_mutex_file_requests);
 }
 
 void logger_changes_write(const char* fmt, ...)
 {
-    pthread_mutex_lock(&g_mutex_file_changes);
+    pthread_mutex_lock(&s_mutex_file_changes);
     va_list args;
     va_start(args, fmt);
     logger_write(STATE_DIR_FILE_CHANGES_LOG, fmt, args);
     va_end(args);
-    pthread_mutex_unlock(&g_mutex_file_changes);
+    pthread_mutex_unlock(&s_mutex_file_changes);
 }
 
 int logger_changes_sumtime(char* buffer, int buffer_size, const char* pattern)
 {
-    pthread_mutex_lock(&g_mutex_file_changes);
+    pthread_mutex_lock(&s_mutex_file_changes);
     FILE* f = fopen(STATE_DIR_FILE_CHANGES_LOG, "r");
     if (f == NULL) {
         perror("Failed to open log file");
-        pthread_mutex_unlock(&g_mutex_file_changes);
+        pthread_mutex_unlock(&s_mutex_file_changes);
         return -1;
     }
 
@@ -132,6 +132,6 @@ int logger_changes_sumtime(char* buffer, int buffer_size, const char* pattern)
 
     c += snprintf(buffer+c, buffer_size, " (%lu sec)", total_seconds);
 
-    pthread_mutex_unlock(&g_mutex_file_changes);
+    pthread_mutex_unlock(&s_mutex_file_changes);
     return c;
 }
