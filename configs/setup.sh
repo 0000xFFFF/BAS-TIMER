7c185b661d343c5bc3aaaae22fd8f2546113af9a
0000xFFFF <coldmailgg@gmail.com>
2025-11-02 02:51:31 +0100

    config

--- configs/setup.sh
+++ configs/setup.sh
@@ -3,6 +3,7 @@ mkdir -p ~/.config
 mkdir -p ~/.config/i3
 mkdir -p ~/.local/share
 ln -sfr .xinitrc ~/.xinitrc
+ln -sfr .bashrc_start ~/.bashrc_start
 ln -sfr .config/i3/config ~/.config/i3/config
 ln -sfr .config/alacritty ~/.config/.
 ln -sfr .config/konsolerc ~/.config/.
