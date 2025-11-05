168d389c749a73691c1a89343c2966a4cab37c32
0000xFFFF <coldmailgg@gmail.com>
2025-11-05 11:28:12 +0100

    fix mistake

--- static/mobile.js
+++ static/mobile.js
@@ -34,4 +34,4 @@ window.addEventListener('load', zoomOutOnMobile);
 const metaViewport = document.createElement('meta');
 metaViewport.name = 'viewport';
 metaViewport.content = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no';
-document.head.appendChild(metaViewport);wport);
+document.head.appendChild(metaViewport);
