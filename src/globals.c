8a2f285ea47505171d9955d3bcbdfcaf878e8129
0000xFFFF <coldmailgg@gmail.com>
2025-11-25 13:47:02 +0100

    bigger timeout

--- src/globals.c
+++ src/globals.c
@@ -41,8 +41,8 @@ const int SLEEP_MS_WTTRIN = 60 * 5 * 1000; // 5 min
 const int SLEEP_MS_WTTRIN_RETRY = 5000;    // 5 sec
 const int SLEEP_MS_SAVE_INFOS = 3000;      // 3 sec
 
-const uint64_t TIMEOUT_MS_BAS = 1500;
-const uint64_t TIMEOUT_MS_WTTRIN = 10000;
+const uint64_t TIMEOUT_MS_BAS = 3000;
+const uint64_t TIMEOUT_MS_WTTRIN = 2 * 60 * 1000;
 
 const double TEMP_MIN_SOLAR = 0;
 const double TEMP_MAX_SOLAR = 100;
